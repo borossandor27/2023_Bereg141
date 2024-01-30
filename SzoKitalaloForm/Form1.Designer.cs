9d256ec59c4c79d7777c02b42a9c932c409fb285
borossandor27 <boros.sanyi@gmail.com>
2024-01-30 22:16:54 +0100

    felulet ok

--- SzoKitalaloForm/Form1.Designer.cs
+++ SzoKitalaloForm/Form1.Designer.cs
@@ -29,45 +29,87 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Jatek));
-            this.panel_Abc = new System.Windows.Forms.Panel();
-            this.panel_KitalaltSzo = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.beállításokToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.szavakToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.szinekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel_Abc
+            // menuStrip1
             // 
-            this.panel_Abc.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel_Abc.Location = new System.Drawing.Point(0, 263);
-            this.panel_Abc.Name = "panel_Abc";
-            this.panel_Abc.Size = new System.Drawing.Size(850, 192);
-            this.panel_Abc.TabIndex = 0;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beállításokToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(933, 33);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // panel_KitalaltSzo
+            // beállításokToolStripMenuItem
             // 
-            this.panel_KitalaltSzo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_KitalaltSzo.Location = new System.Drawing.Point(0, 0);
-            this.panel_KitalaltSzo.Name = "panel_KitalaltSzo";
-            this.panel_KitalaltSzo.Size = new System.Drawing.Size(850, 86);
-            this.panel_KitalaltSzo.TabIndex = 1;
+            this.beállításokToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.szavakToolStripMenuItem,
+            this.szinekToolStripMenuItem});
+            this.beállításokToolStripMenuItem.Name = "beállításokToolStripMenuItem";
+            this.beállításokToolStripMenuItem.Size = new System.Drawing.Size(111, 29);
+            this.beállításokToolStripMenuItem.Text = "Beállítások";
+            // 
+            // szavakToolStripMenuItem
+            // 
+            this.szavakToolStripMenuItem.Name = "szavakToolStripMenuItem";
+            this.szavakToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.szavakToolStripMenuItem.Text = "Szavak";
+            // 
+            // szinekToolStripMenuItem
+            // 
+            this.szinekToolStripMenuItem.Name = "szinekToolStripMenuItem";
+            this.szinekToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.szinekToolStripMenuItem.Text = "Szinek";
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 33);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 3;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(933, 476);
+            this.tableLayoutPanel.TabIndex = 4;
             // 
             // Form_Jatek
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(850, 455);
-            this.Controls.Add(this.panel_KitalaltSzo);
-            this.Controls.Add(this.panel_Abc);
+            this.ClientSize = new System.Drawing.Size(933, 509);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_Jatek";
             this.Text = "Szókitaláló játék";
             this.Load += new System.EventHandler(this.Form_Jatek_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel_Abc;
-        private System.Windows.Forms.Panel panel_KitalaltSzo;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem beállításokToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem szavakToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem szinekToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
     }
 }
 
--- SzoKitalaloForm/Form1.cs
+++ SzoKitalaloForm/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Windows;
 
 namespace SzoKitalaloForm
 {
@@ -18,6 +19,9 @@ namespace SzoKitalaloForm
         Random rand = new Random();
         int buttonSize = 30;
         string kitalaltSzo = string.Empty;
+        Panel panel_tipp = new Panel();
+        Panel panel_Abc = new Panel();
+        Panel panel_KitalaltSzo = new Panel();
         public Form_Jatek()
         {
             InitializeComponent();
@@ -25,31 +29,64 @@ namespace SzoKitalaloForm
 
         private void Form_Jatek_Load(object sender, EventArgs e)
         {
+            tableLayoutPanel.AutoSize = true;
+            
+            Label feliratGondoltSzo = new Label();
+            feliratGondoltSzo.Text = "Gondolt szó:";
+            feliratGondoltSzo.Dock = DockStyle.Fill;
+            feliratGondoltSzo.TextAlign=ContentAlignment.TopRight;
+            tableLayoutPanel.Controls.Add(feliratGondoltSzo, 0, 0);
+            tableLayoutPanel.Controls.Add(panel_KitalaltSzo);
+            Label feliratTipp= new Label();
+            feliratTipp.Text = "Az ön tippje:";
+            feliratTipp.Dock = DockStyle.Fill;  
+            feliratTipp.TextAlign=ContentAlignment.TopRight;
+            tableLayoutPanel.Controls.Add(feliratTipp, 0,1);
+            tableLayoutPanel.Controls.Add(panel_tipp, 1, 1);
+            panel_Abc.Dock = DockStyle.Fill;
+
+            tableLayoutPanel.Controls.Add(panel_Abc, 1, 2);
+            
             SzavakBeolvasasa();
             gondolokEgySzora();
             abctKipakol();
+            uresTippek();
+        }
+
+        private void uresTippek()
+        {
+             panel_tipp.Controls.Clear();
+            for (int i = 0; i < kitalaltSzo.Length; i++)
+            {
+                Button betuButton = new Button();
+                betuButton.Tag = string.Empty;
+                betuButton.Text = "_";
+                betuButton.SetBounds(i * buttonSize, 0, buttonSize, buttonSize);
+                panel_tipp.Controls.Add(betuButton);
+            }
+            Button tippButton = new Button();
+            tippButton.Tag = string.Empty;
+            tippButton.Text = "Tippelek";
+            tippButton.AutoSize = true;
+            tippButton.Location = new Point(kitalaltSzo.Length * buttonSize+40, 0);
+            tippButton.Size = new Size(200, buttonSize);
+            panel_tipp.Controls.Add(tippButton);
+            panel_tipp.Dock = DockStyle.Fill;
         }
 
         private void abctKipakol()
         {
             panel_Abc.Controls.Clear();
-            int betuPerSor = 4;
+            int betuPerSor = 10;
 
             for (int i = 0; i < abc.Length; i++)
             {
+                int sor = (int)i / betuPerSor;
                 Button betuButton = new Button();
                 betuButton.Tag = abc[i];
                 betuButton.Text = abc[i];
                 
-                if (i%betuPerSor == 0)
-                {
-                    j = 0;
-                }
-                else
-                {
-                    j++;
-                }
-                betuButton.SetBounds(i * buttonSize, j * buttonSize, buttonSize, buttonSize);
+                betuButton.SetBounds((i-sor*betuPerSor) * buttonSize, sor * buttonSize, buttonSize, buttonSize);
                 panel_Abc.Controls.Add(betuButton);
             }
         }
@@ -75,7 +112,7 @@ namespace SzoKitalaloForm
             string szavakFajl = "szavak.txt";
             if (!File.Exists(szavakFajl))
             {
-                MessageBox.Show("Nincsenek kitalálható szavak!");
+                System.Windows.Forms.MessageBox.Show("Nincsenek kitalálható szavak!");
                 Environment.Exit(0);
             }
             using (StreamReader sr = new StreamReader(szavakFajl))
