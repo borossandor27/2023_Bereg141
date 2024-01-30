69bd83170ad604dec1be0807565422baf16f7f0d
borossandor27 <boros.sanyi@gmail.com>
2024-01-30 11:43:46 +0100

    jatek

---
+++ SzoKitalaloForm/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SzoKitalaloForm
+{
+    partial class Form_Jatek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Jatek));
+            this.panel_Abc = new System.Windows.Forms.Panel();
+            this.panel_KitalaltSzo = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel_Abc
+            // 
+            this.panel_Abc.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_Abc.Location = new System.Drawing.Point(0, 263);
+            this.panel_Abc.Name = "panel_Abc";
+            this.panel_Abc.Size = new System.Drawing.Size(850, 192);
+            this.panel_Abc.TabIndex = 0;
+            // 
+            // panel_KitalaltSzo
+            // 
+            this.panel_KitalaltSzo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_KitalaltSzo.Location = new System.Drawing.Point(0, 0);
+            this.panel_KitalaltSzo.Name = "panel_KitalaltSzo";
+            this.panel_KitalaltSzo.Size = new System.Drawing.Size(850, 86);
+            this.panel_KitalaltSzo.TabIndex = 1;
+            // 
+            // Form_Jatek
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 455);
+            this.Controls.Add(this.panel_KitalaltSzo);
+            this.Controls.Add(this.panel_Abc);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_Jatek";
+            this.Text = "Szókitaláló játék";
+            this.Load += new System.EventHandler(this.Form_Jatek_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_Abc;
+        private System.Windows.Forms.Panel panel_KitalaltSzo;
+    }
+}
+
---
+++ SzoKitalaloForm/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SzoKitalaloForm
+{
+    public partial class Form_Jatek : Form
+    {
+        string[] abc = { "A", "Á", "B", "C", "D", "E", "É", "F", "G", "H", "I", "Í", "J", "K", "L", "M", "N", "O", "Ó", "Ö", "Ő", "P", "R", "S", "T", "U", "Ú", "Ü", "Ű", "V", "Z" };
+        string[] szavak;
+        Random rand = new Random();
+        int buttonSize = 30;
+        string kitalaltSzo = string.Empty;
+        public Form_Jatek()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Jatek_Load(object sender, EventArgs e)
+        {
+            SzavakBeolvasasa();
+            gondolokEgySzora();
+            abctKipakol();
+        }
+
+        private void abctKipakol()
+        {
+            panel_Abc.Controls.Clear();
+            int betuPerSor = 4;
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Button betuButton = new Button();
+                betuButton.Tag = abc[i];
+                betuButton.Text = abc[i];
+                
+                if (i%betuPerSor == 0)
+                {
+                    j = 0;
+                }
+                else
+                {
+                    j++;
+                }
+                betuButton.SetBounds(i * buttonSize, j * buttonSize, buttonSize, buttonSize);
+                panel_Abc.Controls.Add(betuButton);
+            }
+        }
+
+        private void gondolokEgySzora()
+        {
+            int index = rand.Next(0, szavak.Length);
+            kitalaltSzo = szavak[index];
+            //-- kirakjuk a panelre betűnként ---
+            panel_KitalaltSzo.Controls.Clear();
+            for (int i = 0; i < kitalaltSzo.Length; i++)
+            {
+                Button betuButton = new Button();
+                betuButton.Tag = kitalaltSzo[i];
+                betuButton.Text = "_";
+                betuButton.SetBounds(i * buttonSize, 0, buttonSize, buttonSize);
+                panel_KitalaltSzo.Controls.Add(betuButton);
+            }
+        }
+
+        private void SzavakBeolvasasa()
+        {
+            string szavakFajl = "szavak.txt";
+            if (!File.Exists(szavakFajl))
+            {
+                MessageBox.Show("Nincsenek kitalálható szavak!");
+                Environment.Exit(0);
+            }
+            using (StreamReader sr = new StreamReader(szavakFajl))
+            {
+                szavak = sr.ReadToEnd().Replace("\"", "").Replace(" ", "").Split(',');
+            }
+        }
+    }
+}
