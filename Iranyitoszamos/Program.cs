e28f59639419a6b976dac64008a0cad2deca3689
borossandor27 <boros.sanyi@gmail.com>
2024-03-04 12:13:42 +0100

    mukodik

--- Iranyitoszamos/Program.cs
+++ Iranyitoszamos/Program.cs
@@ -12,16 +12,25 @@ namespace Iranyitoszamos
     internal class Program
     {
         static PostalCode postalCode=null;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string keresettIranyitoszam = "4220";
-            irszam(keresettIranyitoszam);
+            await telepulesAdatok(keresettIranyitoszam);
             Console.WriteLine($"{postalCode.Places[0].PlaceName.ToString()} település irányítószáma: {keresettIranyitoszam}"   );
+            Console.WriteLine($"{postalCode.Places[0].State} megye");
+            Console.WriteLine($" Google Maps link: https://www.google.com/maps/place/{postalCode.Places[0].Latitude},{postalCode.Places[0].Longitude}");
             Console.WriteLine("vége");
             Console.ReadLine();
         }
-        private static async void irszam(string irszam)
+        private static async Task telepulesAdatok(string irszam)
         {
+            /*
+             * "place name": "Hajdúböszörmény",
+             * "longitude": "21.5167",
+             * "state": "Hajdú-Bihar",
+             * "state abbreviation": "HB",
+             * "latitude": "47.6667"
+             */
             string url = $"http://api.zippopotam.us/hu/{irszam}";
             var client = new HttpClient();
             var request = new HttpRequestMessage(HttpMethod.Get, url);
