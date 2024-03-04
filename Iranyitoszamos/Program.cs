590be446a61f8f4ae7e075963ffc5bce4b4cb17d
borossandor27 <boros.sanyi@gmail.com>
2024-03-04 10:52:27 +0100

    parameterezve

--- Iranyitoszamos/Program.cs
+++ Iranyitoszamos/Program.cs
@@ -4,26 +4,31 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using Iranyitoszamos;
 
 namespace Iranyitoszamos
 {
     internal class Program
     {
+        static PostalCode postalCode=null;
         static void Main(string[] args)
         {
-            irszam();
+            string keresettIranyitoszam = "4220";
+            irszam(keresettIranyitoszam);
+            Console.WriteLine($"{postalCode.Places[0].PlaceName.ToString()} település irányítószáma: {keresettIranyitoszam}"   );
             Console.WriteLine("vége");
             Console.ReadLine();
         }
-       private static async  void irszam()
+        private static async void irszam(string irszam)
         {
-            
-                var client = new HttpClient();
-                var request = new HttpRequestMessage(HttpMethod.Get, "http://api.zippopotam.us/us/98121");
-                var response = await client.SendAsync(request);
-                response.EnsureSuccessStatusCode();
-                Console.WriteLine(await response.Content.ReadAsStringAsync());
-
+            string url = $"http://api.zippopotam.us/hu/{irszam}";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string jsonString = await response.Content.ReadAsStringAsync();
+            postalCode = PostalCode.FromJson(jsonString);
             
         }
 
