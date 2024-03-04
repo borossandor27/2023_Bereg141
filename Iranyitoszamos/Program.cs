4efdf3892c9b337da32c0f1e220703584c99edb0
borossandor27 <boros.sanyi@gmail.com>
2024-03-04 10:24:07 +0100

    irszamos

---
+++ Iranyitoszamos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iranyitoszamos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            irszam();
+            Console.WriteLine("vége");
+            Console.ReadLine();
+        }
+       private static async  void irszam()
+        {
+            
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://api.zippopotam.us/us/98121");
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+            
+        }
+
+    }
+}
